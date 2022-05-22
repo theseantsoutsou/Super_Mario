25522ac4501e4e41d0c38a49a692323f057ab1f4
ConnorMcCloudGibson <connor.mccloud.gibson@gmail.com>
2022-05-22 21:07:16 +1000

    Java Docs

--- src/game/behaviours/SpeechBehaviour.java
+++ src/game/behaviours/SpeechBehaviour.java
@@ -6,13 +6,31 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import game.actions.SpeechAction;
 import game.actors.Speaks;
 
+/**
+ * SpeechBehaviour allows actors to say their monologue by generating a SpeechAction every 3second turn.
+ * @author Connor McCloud-Gibson
+ * @since 15/05/22
+ */
 public class SpeechBehaviour implements Behaviour {
+    /**
+     * Actor that is speaking
+     */
     Speaks speaker;
     int turns = -1;
+    /**
+     * Constructor
+     */
     public SpeechBehaviour(Speaks speaker){
         this.speaker = speaker;
 
     }
+
+    /**
+     * Get the SpeakAction every even amount of turns
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return SpeechAction
+     */
     public Action getAction(Actor actor, GameMap map) {
         turns+=1;
         if(turns%2==0){
